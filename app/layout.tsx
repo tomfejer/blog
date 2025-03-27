8c25cdf436fad9defb11c28f69b934bdadfa0432
tomfejer <grotandthemob@gmail.com>
2025-03-27 21:44:51 +0100

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,11 +54,11 @@ export default function RootLayout({
     >
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-          <Navbar />
+          {/* <Navbar /> */}
           {children}
-          <Footer />
-          <Analytics />
-          <SpeedInsights />
+          {/* <Footer /> */}
+          {/* <Analytics /> */}
+          {/* <SpeedInsights /> */}
         </main>
       </body>
     </html>
