68b2e208278e5348422091f4eb59e81348d2605c
tomfejer <grotandthemob@gmail.com>
2025-03-29 20:55:22 +0100

    more links and bio

--- app/layout.tsx
+++ app/layout.tsx
@@ -79,8 +79,8 @@ export default function RootLayout({
           }}
         />
       </head>
-      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+      <body className="antialiased absolute inset-0">
+        <main>
           {/* <Navbar /> */}
           {children}
           {/* <Footer /> */}
