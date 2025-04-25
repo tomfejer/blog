a1197b8031f8b99efd904a28db0f20ceef1f562f
tomfejer <grotandthemob@gmail.com>
2025-04-26 00:30:41 +0200

    simplify

--- app/page.tsx
+++ app/page.tsx
@@ -5,25 +5,25 @@ import { InstagramIcon, ThreadsIcon, LinkedInIcon, UnsplashIcon } from './compon
 export default function Page() {
   return (
     <section className="absolute inset-0 flex items-center justify-center h-screen">
-      <TiltContainer>
-        <div className="p-4 m-4 bg-neutral-900 rounded-xl border border-neutral-800 min-w-[300px] flex gap-10 items-start justify-between hover:scale-105 transition-all duration-300 active:scale-95">
+      {/* <TiltContainer> */}
+        {/* <div className="p-4 m-4 bg-neutral-900 rounded-xl border border-neutral-800 min-w-[300px] flex gap-10 items-start justify-between hover:scale-105 transition-all duration-300 active:scale-95"> */}
           <div>
             <h1 className="text-2xl font-bold">
-              Tom Fejer
+              Tom Fejér
             </h1>
             <p className="text-neutral-500 text-sm pt-1">
-              Product Designer
+            ⚡︎ Designer
             </p>
-            <p className='max-w-[300px] text-neutral-100 text-sm pt-2'>Coffee, comics, and crafting awesome things. Also, raising two future superheroes.</p>
+            <p className='max-w-[300px] text-neutral-100 text-sm pt-2'>Powered by coffee, building products, and chasing two tiny superheroes in my free time.</p>
           </div>
           <div>
             <div className="flex items-center gap-4 pt-1">
-              <a href="https://www.instagram.com/grotandthemob/" target="_blank" rel="noopener noreferrer" title="Instagram" className='opacity-40 hover:opacity-100 transition-opacity duration-300'>
+              {/* <a href="https://www.instagram.com/grotandthemob/" target="_blank" rel="noopener noreferrer" title="Instagram" className='opacity-40 hover:opacity-100 transition-opacity duration-300'>
                 <InstagramIcon />
-              </a>
-              <a href="https://www.threads.net/@grotandthemob" target="_blank" rel="noopener noreferrer" title="Threads" className='opacity-40 hover:opacity-100 transition-opacity duration-300'>
+              </a> */}
+              {/* <a href="https://www.threads.net/@grotandthemob" target="_blank" rel="noopener noreferrer" title="Threads" className='opacity-40 hover:opacity-100 transition-opacity duration-300'>
                 <ThreadsIcon />
-              </a>
+              </a> */}
               {/* <a href="https://www.linkedin.com/in/tomfejer/" target="_blank" rel="noopener noreferrer" title="LinkedIn" className='opacity-40 hover:opacity-100 transition-opacity duration-300'>
                 <LinkedInIcon />
               </a>
@@ -32,8 +32,8 @@ export default function Page() {
               </a> */}
             </div>
           </div>
-        </div>
-      </TiltContainer>
+        {/* </div> */}
+      {/* </TiltContainer> */}
       {/* <div className="my-8"> */}
       {/* <BlogPosts /> */}
       {/* </div> */}
